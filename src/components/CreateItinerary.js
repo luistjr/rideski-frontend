ec7d93fa33a7346e26022fd38d8413ed130a0d8a
therunningagent <luistjr@gmail.com>
2021-03-03 10:36:19 -0700

    added create user itinerary instance

--- src/components/CreateItinerary.js
+++ src/components/CreateItinerary.js
@@ -6,7 +6,8 @@ function CreateItinerary({ user, setShowHome }) {
 
   const toggleHome = setShowHome(false)
   const [date, setDate] = useState(new Date());
-  const [showCalendar, setShowCalendar] = useState(false)
+  const [showCalendar, setShowCalendar] = useState(false);
+  const [rI, setRI] = useState(null);
 
   const month = date.getUTCMonth() + 1;
   const day = date.getUTCDate();
@@ -32,31 +33,45 @@ function CreateItinerary({ user, setShowHome }) {
       body: JSON.stringify(itineraryDate),
     })
       .then(response => response.json())
-      .then(data => {
-        console.log('Success:', data);
-      });
+      .then((data) => createUserItinerary(data.id));
   }
 
-  function handleDateClick() {
-    setShowCalendar(true)
+  function createUserItinerary(id) {
+
+    fetch('http://[::1]:3001/user_itineraries/', {
+      method: 'POST',
+      headers: {
+        'Content-Type': 'application/json',
+      },
+      body: JSON.stringify({
+        user_id: user.id,
+        itinerary_id: id
+      }),
+    })
+      .then(response => response.json())
+      .then(data => console.log("UI", data));
   }
 
 
-  return (
-    <div>
-      {toggleHome}
-      <br />
-      <button onClick={handleDateClick}>Select Date</button>
-      <br />
-      <br />
-      {showCalendar ? <form onSubmit={handleSubmit}>
-        <Calendar onChange={setDate} value={date} />
-        <br />
-        <input type="submit" />
-      </form> : null}
-    </div>
+function handleDateClick() {
+  setShowCalendar(true)
+}
 
-  );
+return (
+  <div>
+    {toggleHome}
+    <br />
+    <button onClick={handleDateClick}>Select Date</button>
+    <br />
+    <br />
+    {showCalendar ? <form onSubmit={handleSubmit}>
+      <Calendar onChange={setDate} value={date} />
+      <br />
+      <input type="submit" />
+    </form> : null}
+  </div>
+
+);
 }
 
 export default CreateItinerary;
